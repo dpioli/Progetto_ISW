45c6609fbdc3c0d40d8972253bc6c9630dbc0191
ErjonaMaxhaku <104934938+ErjonaMaxhaku@users.noreply.github.com>
2025-04-07 17:54:38 +0200

    FDCultimo

--- Progetto_ISW/src/applicazione/FatConversione.java
+++ Progetto_ISW/src/applicazione/FatConversione.java
@@ -16,59 +16,54 @@ public class FatConversione {
 	}
 	public FatConversione() {
 		this.fdc = new ArrayList<ArrayList<Double>>();
+		ArrayList<Double> id = new ArrayList<>();
+		id.add(00.00);
+		fdc.add(id);
+	}
+	/**
+	 * Metodo che aggancia una nuova foglia alle altre (partendo sempre da F1).
+	 * @param ID nuova aggiunta
+	 */
+	public void aggancia(Integer nuova) {
+		Double id = nuova.doubleValue();	
+
+		aggiungiRiga(id);
+		aggiungiColonna(id);
+		//System.out.println("prima dell'aggiornamento di valori:\n\n " + fdc.toString() );
+		aggiornaValori();
 	}
 	
 	private void aggiungiRiga(Double id) {
+		fdc.get(0).add(id);
+		
 		ArrayList<Double> nuovaRiga = new ArrayList<>();
-		nuovaRiga.add(id);
-		for(int i = 1; i < fdc.size(); i++) {
+		// devo aggiungere NUOVO e1 e2 e3 ... 0.0
+		nuovaRiga.add(id); //aggiungo NUOVO
+		for(int i = 1; i < fdc.size(); i++) { //aggiungo i valori impostati a 0.0(default) in corrispondenza di quelli già esistenti
 			nuovaRiga.add(0.0);
 		}
 		fdc.add(nuovaRiga);
 	}
 	
-	private void aggiungiColonna(Double id) {
-		fdc.get(0).add(id);
-		for(int i = 1; i < fdc.size(); i++) {
+	private void aggiungiColonna(Double id) {   //QUI fdc.size() corrisponde a fdc con la riga aggiunta
+		//fdc.get(0).add(id); //alla prima riga (degli ID) aggiungo quello nuovo => nuova colonna
+	//	System.out.println("aggiungo id in colonna" + fdc.toString());
+		for(int i = 1; i < fdc.size(); i++) { //completo la colonna con valori impostati a zero
 			fdc.get(i).add(0.0);
 		}
 	}
+	
 	/**
-	 * per calcolare fdc(Gerarchia nuova, Gerarchia bersaglio, fdc)
-	 * se ho solo due foglie
-	 * fdc12 = k;
-	 * fdc21 = 1/k;
-	 * se ho tre foglie
-	 * fdc13 = k;
-	 * fdc31 = 1/k;
-	 * fdc 23 = fdc21 * fdc 13 COMPRESO TRA 0.5 e 2
-	 * SE
-	 * fdc12
-	 * fdc13 = fdc23/fdc21
-	 * max fdc13 = 2 / fdc21
-	 * min fdc13 = 0.5 / fdc21
-	 * 
-	 * se avessi 4 foglie => i rami uscenti da 1 sono 3:
-	 * uno lo scelgo io, gli altri due sono x,y
-	 * fdc14 ?
-	 * max fdc14 = 2*min(1,x,y)
-	 * min fdc14 = 0.5max(1,x,y)
-	 * 
-	 * se avessi n foglie
-	 * n-1 rami da n
-	 * fdc n
-	 * max = 2*min(1,x)
-	 * min = 0.5*max(1,x)
-	 * @return
+	 * Questo metodo permette di calcolare il massimo che può assumere il FDC.
+	 * Se considero F1,F2 nodi di cui ho i fattori di conversione. OUTesistente 2->1
+	 * Inserisco il nuovo nodo F3.
+	 * OUTnuovo 2 -> 3, sarà un valore compreso tra 0,5 e 2
+	 * Sapendo che la transizione 2 -> 3 = ( TR 2->1 ) * ( TR 1->3 )
+	 * ( TR 1 -> 3 ) =   OUTnuovo/ OUTesistente
+	 * Il discorso è analogo nel verso opposto.
+	 * @param ramiUscenti dal primo nodo
+	 * @return il valore minimo tra 2.0 e il massimo calcolato
 	 */
-	/*
-private Double calcolaLimiteMax(ArrayList<Tupla<Gerarchia, Double>> tuttiIFattoriUscentiDaFoglia1, Double maxFdc) {
-Double max=maxFdc;
-for(Tupla<Gerarchia,Double> x:tuttiIFattoriUscentiDaFoglia1) {
-max=Math.min(max, maxFdc/x.getSecond());
-}
-return max;
-}*/
 	private double calcolaMAX(ArrayList<Double> ramiUscenti) {
 		Double max = MAX_FDC;
 		for(Double d : ramiUscenti) {
@@ -77,96 +72,83 @@ return max;
 		}
 		return max;		
 	}
-	/*
-	private Double calcolaLimiteMin(ArrayList<Tupla<Gerarchia, Double>> tuttiIFattoriUscentiDaFoglia1, Double minFdc) {
-	Double min=minFdc;
-	for(Tupla<Gerarchia,Double> x:tuttiIFattoriUscentiDaFoglia1) {
-	min=Math.max(min, minFdc/x.getSecond());
-	}
-	return min;
-
-	}*/
+	
+	/**
+	 * Questo metodo permette di calcolare il minimo che può assumere il FDC.
+	 * Se considero F1,F2 nodi di cui ho i fattori di conversione. OUTesistente 2->1
+	 * Inserisco il nuovo nodo F3.
+	 * OUTnuovo 2 -> 3, sarà un valore compreso tra 0,5 e 2
+	 * Sapendo che la transizione 2 -> 3 = ( TR 2->1 ) * ( TR 1->3 )
+	 * ( TR 1 -> 3 ) =   OUTnuovo/ OUTesistente
+	 *  Il discorso è analogo nel verso opposto.
+	 * @param ramiUscenti dal primo nodo
+	 * @return il valore massimo tra 0.5 e il massimo calcolato
+	 */
 	private double calcolaMIN(ArrayList<Double> ramiUscenti) {
 		Double min = MIN_FDC;
 		for(Double d: ramiUscenti) {
-			if( d != 0.0)
+			if( d != 0.0)  //ignoro i valori di default
 				min = Math.max(min, MIN_FDC / d);
 		}
 		return min;
 	}
 	
-	public void aggancia(Integer nuova) {
-		Double id = nuova.doubleValue();	
-		System.out.println("Id vale    " + id);
-		aggiungiRiga(id);
-		aggiungiColonna(id);
-		aggiornaValori();
-		
-	}
 
-	private void aggiornaValori() {
-		int cont = fdc.size() - 1;
-		System.out.println("cont vale   " + cont);
-		//GGIUNGO COUNTER numero = ++numero ogni volta che devo agganciare una Integer nuova
-		//hash map tra id Integer nuova e counter del numero che sto aggiungendo
+
+	private void aggiornaValori() { //ATTENZIONE LA POSIZIONE 0 (riga e colonna) E' OCCUPATA DAGLI id
 		
-		ArrayList<Double> ramiUscenti = fdc.get(cont);
+		int cont = fdc.size() - 1; //punto a prendere il nuovo arrivato**
+		System.out.println("cont vale   " + cont);
+		if(cont == 1) return;
+		
+		ArrayList<Double> ramiUscenti = fdc.get(1); //prendo rami uscenti dalla prima foglia che userò sempre come riferimento
 		
 		double max = calcolaMAX(ramiUscenti);
 		double min = calcolaMIN(ramiUscenti);
+		System.out.printf("\nMIN : %f \nMAX : %f\n", min, max);
 		double k = InputDati.leggiDoubleConMINeMAX("Inserire valore fattore di conversione associato a 1 or whateva", min, max);
-		//LAVORO SU RIGA
+		
 		//aggancio la prima quindi posizione 0
-		fdc.get(cont).set(0, k);
-		fdc.get(0).set(cont, 1 / k);
+		fdc.get(cont).set(1, k); 
+		//RICAVO INVERSO
+		fdc.get(1).set(cont, 1 / k);
+		System.out.println("\nper ora : \n" + fdc.toString());
 
-		//LAVORO SU COLONNA
-		for(int i = 0; i < cont; i++) {
+		// COMPLETO SAPENDO CHE LE TRANSIZIONI SONO PRODOTTO DEL NUOVO INSERIMENTO (k) CON USCENTI DALLA PRIMA FOGLIA
+		for(int i = 2; i < cont; i++) {
 			//fdc[nuova][i] = fdc[nuova][1]*fdc[1][i];
-			double valore = fdc.get(cont).get(0) * fdc.get(0).get(i);
+			//double valore = fdc.get(cont).get(0) * fdc.get(0).get(i);
+			double valore = k * ramiUscenti.get(i);
 			fdc.get(cont).set(i, valore);
+			//COMPLETO LA COLONNA CON L'INVERSO
 			//fdc[i][nuova] = 1/fdc[nuova][i];
 			double invertito = 1 / valore;
 			fdc.get(i).set(cont, invertito);
 		}
-		completaVuote();
+		fdc.get(cont).set(cont, 0.0); //completo la diagonale
+		//completaVuote();
+		System.out.println("\naggiornati i valori risulta:\n" + fdc.toString());
 	}
 	
-	private void completaVuote() {
+/*	private void completaVuote() { //in teoria non serve wtf
 		
 		for(int i = 1; i < fdc.size(); i++) {
 			for(int j = 1; j < fdc.size(); j++) {
+				if ( i == j )
+					fdc.get(i).set(j, 0.0);
 				if (fdc.get(i).get(j) == null)
 						fdc.get(i).set(j, 0.0);
 			}
 		}
 		
 	}
-
+*/
 	public void stampaFDC() {
 		if(fdc.equals(null))
 			System.out.println("Nessun fattore di conversione presente");
 		else
 			System.out.println(this.toString());
 	}
-	/**
-	 
-	//array[categorieFogli.length()][categorieFogli.length()]
-	
-	     array[1][3]= array[1][2]*array[2][3]
-	per i che va da 1 a nuova-1
-	array[i][nuova] = array[i][i+1]*array[i+1][nuova]
-
-	private void recupera() {
-		//inverso
-		inserisci(1/k, rispetto, d);
-		//estraggo array[di][]
-		for(int i = 1; i < array[di].length(); i++) {
-			
-		}
-	}
-	*/
-	//HashMap<CategoriaFoglia, double>();
 	
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
